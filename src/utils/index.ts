3d199bae7d60fa467b1225e9dbc4c0867a856f6c
ygl <lanyueos@qq.com>
2019-06-10 12:32:02 +0800

    增加isArr, 增加 format 功能

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,23 +4,28 @@ export function escapeId (value: string, dotQualifier?: boolean) : string {
   return SqlString.escapeId(value, dotQualifier)
 }
 
-export function escape (value: string): string {
+export function escape (value: any): string {
   return SqlString.escape(value)
 }
 
-export function format (sql: string, args: { [key: string]: any } | Array<string | string>): string {
-  return sql.replace(/\:(\w+)/g, function (txt: string, key: string | number) {
+export function format (sql: string, args: { [ key: string  ]: any; [key: number]: any } | Array<any>): string {
+  // Support :arg and :?arg placeholder
+  const result = sql.replace(/\:\??(\w+)/g, function (txt: string, key: any) {
     if (args.hasOwnProperty(key)) {
-      // return SqlString.escape(args[key])
+      // :?arg use escapeId
+      return txt.indexOf('?') > -1 ? escapeId(args[key]) : escape(args[key])
     }
     return txt
   })
+  // Support ? placeholder
+  return SqlString.format(result, args)
 }
 
 export function raw (sql: string): { toSqlString: () => string } {
   return SqlString.raw(sql)
 }
 
+// Remove the blanks at both ends of the string
 export function trim (str: string): string {
   return str.replace(/^\s*|\s*$/g, '')
 }
@@ -32,4 +37,8 @@ export function alias (field: string, aliasName: string, isRaw?: boolean): strin
 export function escapeField (value: string, isRaw?: boolean): string {
   const [field, aliasName] = value.split(/\s+as\s+/)
   return alias(field, aliasName, isRaw)
+}
+
+export function isArr (value: any): boolean {
+  return Object.prototype.toString.call(value) === '[object Array]'
 }
