7626c2e2edb2825cfe473392f255d564501708cd
ygl <lanyueos@qq.com>
2019-06-14 16:31:07 +0800

    实现 select 函数

--- src/builder.ts
+++ src/builder.ts
@@ -1,6 +1,12 @@
 'use strict'
 import * as utils from './utils'
-interface anyObj { [key: string]: string; [key: number]: string }
+
+interface rawSqlType { toSqlString: () => string }
+type fieldAliasType = [string | rawSqlType, string | undefined]
+interface fieldObjType {
+  [key: string]: string | fieldAliasType;
+  [key: number]: string | fieldAliasType
+}
 
 export default class Builder {
   protected $fields: string;
@@ -19,32 +25,50 @@ export default class Builder {
     this.$tableAlias = tableAlias
   }
 
-  select (attrs: anyObj | Array<string>): Builder {
-    let params = []
-    if (utils.isArr(attrs)) {
-      params = (<Array<string>>attrs).map(e => e)
-    } else {
-      for (let key in attrs) {
-        params.push(`${key} as ${attrs[key]}`)
-      }
-    }
+  static raw (sql: string): rawSqlType {
+    return utils.raw(sql)
+  }
 
-    this.$fields = params.map(field => {
-      return utils.escapeField(field)
+  /**
+   * 指定需要查询字段
+   * @param {array<string | fieldAliasType>} attrs
+   * @returns {Builder}
+   * **/
+  select (attrs: Array<string | fieldAliasType> = ['*']): Builder {
+    this.$fields = attrs.map((field) => {
+      if (utils.isArr(field)) {
+        let temp = ''
+        if (utils.isFun((<rawSqlType>field[0]).toSqlString)) {
+          temp += (<rawSqlType>field[0]).toSqlString()
+        } else {
+          temp += utils.escapeField(<string>field[0])
+        }
+
+        if (field[1]) {
+          temp += ` as ${utils.escapeField(field[1])}`
+        }
+        return temp
+      }
+
+      return utils.escapeField((<string>field))
     }).join(',')
 
     this.$operType = 'select'
     return this
   }
+
   create () {
     this.$operType = 'insert'
   }
+
   update () {
     this.$operType = 'update'
   }
+
   delete () {
     this.$operType = 'delete'
   }
+
   where () {}
   having () {}
   order () {}
