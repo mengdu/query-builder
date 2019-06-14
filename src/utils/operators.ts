f9e7d7118797efdb260cacd33f9f99ec7c06063c
ygl <lanyueos@qq.com>
2019-06-14 19:04:41 +0800

    添加操作符函数

---
+++ src/utils/operators.ts
@@ -0,0 +1,17 @@
+'use strict'
+import { escape } from './index'
+
+export default {
+  $gt: (val: any): string => `> ${escape(val)}`,
+  $gte: (val: any): string => `>= ${escape(val)}`,
+  $lt: (val: any): string => `< ${escape(val)}`,
+  $lte: (val: any): string => `<= ${escape(val)}`,
+  $eq: (val: any): string => `= ${escape(val)}`,
+  $neq: (val: any): string => `!= ${escape(val)}`,
+  $between: (arr: [any, any]): string => `between ${escape(arr[0])} and ${escape(arr[1])}`,
+  $notBetween: (arr: [any, any]): string => `not between ${escape(arr[0])} and ${escape(arr[1])}`,
+  $in: (arr: any[]): string => `in(${arr.map(e => escape(e)).join(',')})`,
+  $notIn: (arr: any[]): string => `not in(${arr.map(e => escape(e)).join(',')})`,
+  $like: (val: string): string => `like ${escape(val)}`,
+  $notLike: (val: string): string => `not like ${escape(val)}`
+}
